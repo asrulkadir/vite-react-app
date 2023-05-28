937f62a321612c4e1305c17335177a7407667ac0
Asrul Kadir <asrulkadir.asus4@gmail.com>
2023-05-28 13:47:07 +0800

    add text

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -25,12 +25,13 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <p>Hello Vite</p>
+        <p>This is starting template react app use vite</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
